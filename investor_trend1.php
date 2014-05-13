c343f60cb3278a5b53724391c304b802c35b6c19
Prashanthragh <Prashanth_ragh@ymail.com>
2014-05-12 17:11:52 -0700

    prashanth's changes

--- investor_trend1.php
+++ investor_trend1.php
@@ -17,7 +17,7 @@ if (!$con) {
 
 mysql_select_db($db, $con);
 
-$a=1;
+//$a=1;
 $query1=mysql_query("SELECT count(`company_name`) as number , `company_category_code` FROM `rounds_ca` WHERE `funding_round_type` = 'angel' where `funded_year` = 2012
 group by company_category_code
 order by number 
@@ -35,141 +35,41 @@ $series1['name'] = '2013';
 
 $series2=array();
 $series2['name']='2012';
-if($a==1)
-{
+
+$series4=array();
+$series4['name']='2011';
+
+$series3=array();
+$series3['name']='2014';
+//if($a==1)
+//
 //while($r = mysql_fetch_array($query1))
-    $category1['data'][0] = "software";
-	$category1['data'][1] = "Mobile";
-	$category1['data'][2] = "web";
-	$category1['data'][3] = "enterprise";
+    $category1['data'][0] = "Angel";
+	$category1['data'][1] = "Series-a";
+	$category1['data'][2] = "Series-b";
+	$category1['data'][3] = "Series-c+";
 	
 	
-    $series1['data'][]=100;
-	 $series1['data'][]=200;
-	  $series1['data'][]=50;
-	   $series1['data'][]=300;
+    $series1['data'][]=824;
+	 $series1['data'][]=600;
+	  $series1['data'][]=219;
+	   $series1['data'][]=248;
 	   
-	   $series2['data'][]=1000;
-	 $series2['data'][]=2000;
-	  $series2['data'][]=50;
-	   $series2['data'][]=3000;
-	
-	}
-//}
-else
-{
-//while($r = mysql_fetch_array($query1)) 
- $category1['data'][0] = "software";
-	$category1['data'][1] = "Mobile";
-	$category1['data'][2] = "webb";
-	$category1['data'][3] = "enterprise";
-	
-	
-    $series1['data'][]=100;
-	 $series1['data'][]=200;
-	  $series1['data'][]=50;
-	   $series1['data'][]=300;
+	   $series2['data'][]=774;
+	 $series2['data'][]=456;
+	  $series2['data'][]=164;
+	   $series2['data'][]=243;
 	   
-	   $series2['data'][]=1000;
-	 $series2['data'][]=2000;
-	  $series2['data'][]=50;
-	   $series2['data'][]=3000;
+	   $series3['data'][]=138;
+	 $series3['data'][]=130;
+	  $series3['data'][]=67;
+	   $series3['data'][]=72;
+	   
+	     $series4['data'][]=591;
+	 $series4['data'][]=421;
+	  $series4['data'][]=209;
+	   $series4['data'][]=235;
 	
-}
-
-/*
-$category2 = array();
-$category2['name'] = 'Business_type';
-
-$series2= array();
-$series2['name'] = '2013';
-
-	
-	while($r = mysql_fetch_array($query2)) {
-    $category2['data'][] = $r['Type'];
-    $series2['data'][]=$r['raised'];
-}
-
-$query3=mysql_query("SELECT count( `company_name` ) AS number, `company_category_code` 
-FROM `rounds_ca` 
-WHERE `funding_round_type` = 'angel'
-AND `funded_year` = '2012'
-GROUP BY company_category_code
-ORDER BY number DESC 
-LIMIT 15 ");
-
-$category3 = array();
-$category3['name'] = 'Business_type';
-
-$series3= array();
-$series3['name'] = '2012';
-
-	
-	while($r = mysql_fetch_array($query3)) {
-    $category3['data'][] = $r['Type'];
-    $series3['data'][]=$r['raised'];
-}
-
-$query4=mysql_query("SELECT count( `company_name` ) AS number, `company_category_code` 
-FROM `rounds_ca` 
-WHERE `funding_round_type` = 'series-a'
-GROUP BY company_category_code
-ORDER BY number DESC 
-LIMIT 15 ");
-
-$category1 = array();
-$category1['name'] = 'Business_type';
-
-$series1= array();
-$series1['name'] = 'All';
-
-
-while($r = mysql_fetch_array($query1)) {
-    $category1['data'][] = $r['Type'];
-    $series1['data'][]=$r['raised'];
-	}
-
-$query5=mysql_query("SELECT count( `company_name` ) AS number, `company_category_code` 
-FROM `rounds_ca` 
-WHERE `funding_round_type` = 'series-a'
-AND `funded_year` = '2013'
-GROUP BY company_category_code
-ORDER BY number DESC 
-LIMIT 15 ");
-
-$category2 = array();
-$category2['name'] = 'Business_type';
-
-$series2= array();
-$series2['name'] = '2013';
-
-	
-	while($r = mysql_fetch_array($query2)) {
-    $category2['data'][] = $r['Type'];
-    $series2['data'][]=$r['raised'];
-}
-
-$query3=mysql_query("SELECT count( `company_name` ) AS number, `company_category_code` 
-FROM `rounds_ca` 
-WHERE `funding_round_type` = 'angel'
-AND `funded_year` = '2012'
-GROUP BY company_category_code
-ORDER BY number DESC 
-LIMIT 15 ");
-
-$category3 = array();
-$category3['name'] = 'Business_type';
-
-$series3= array();
-$series3['name'] = '2012';
-
-	
-	while($r = mysql_fetch_array($query3)) {
-    $category3['data'][] = $r['Type'];
-    $series3['data'][]=$r['raised'];
-}
-
-*/
 
 $result = array();
 array_push($result,$category1);
@@ -177,6 +77,8 @@ array_push($result,$series1);
 
 //array_push($result,$category2);
 array_push($result,$series2);
+array_push($result,$series3);
+array_push($result,$series4);
 
 //array_push($result,$category3);
 //array_push($result,$series3);
